aa1894963e2366812d9a2d78edc5dc2bc58b3a96
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2020-04-29 20:10:51 +0200

    Added byte array to/from Guid conversion.

--- Converted/ValueConverters/ByteArrayValueConvertedProvider.cs
+++ Converted/ValueConverters/ByteArrayValueConvertedProvider.cs
@@ -8,6 +8,9 @@ namespace Converted.ValueConverters
         {
             Register((byte[] input, IFormatProvider? formatProvider) => Convert.ToBase64String(input));
             Register((string input, IFormatProvider? formatProvider) => Convert.FromBase64String(input));
+            
+            Register((byte[] input, IFormatProvider? formatProvider) => new Guid(input));
+            Register((Guid input, IFormatProvider? formatProvider) => input.ToByteArray());
         }
     }
 }
